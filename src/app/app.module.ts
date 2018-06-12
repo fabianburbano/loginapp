98cc14413489ed78783c9264d9f647a790f7325e
fabian <fabr2008@hotmail.com>
2018-06-12 15:18:55 -0500

    Creado el servicio de Auth y el registro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +12,12 @@ import { RegisterPageComponent } from './components/register-page/register-page.
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { PrivadoPageComponent } from './components/privado-page/privado-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+//Servicios
+import { AuthService } from './services/auth.service';
+
+//Environment
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,9 +31,12 @@ import { NotFoundPageComponent } from './components/not-found-page/not-found-pag
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
+public email:string;
+public password:string;
 
-  constructor() { }
+  constructor(public as:AuthService,
+              public router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmitAddUser(){
+    this.as.registerUser(this.email, this.password)
+    .then((res) => {
+      this.router.navigate(['/privado']);
+    }).catch((err) => {
+      console.log('algo va mal');
+      console.log(err);
+    });
   }
 
 }
