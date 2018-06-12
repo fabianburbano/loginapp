df0590fc0ce2f9a719e5f72178c8703a75f8adfb
fabian <fabr2008@hotmail.com>
2018-06-12 15:51:22 -0500

    Añadimos login y logout

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  public email:string;
+  public password:string;
 
-  constructor() { }
+  constructor(public as:AuthService,
+              public router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin(){
+    this.as.loginEmail(this.email, this.password)
+    .then((res) => {
+      console.log('se logueo bien');
+      this.router.navigate(['/privado']);
+    }).catch((err) => {
+      console.log('algo va mal');
+      this.router.navigate(['/login']);
+    });
   }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public isLogin:boolean;
+  public nombreUsuario:string;
+  public emailUsuario:string;
 
-  constructor() { }
+  constructor(public as:AuthService,
+              public router:Router) { }
 
   ngOnInit() {
+    this.as.getAuth().subscribe(auth=>{
+      if(auth){
+        this.isLogin=true;
+        this.nombreUsuario=auth.displayName;
+        this.emailUsuario=auth.email;
+      }else{
+        this.isLogin=false;
+      }
+    });
+  }
+
+  onClickLogout(){
+    this.as.logout();
   }
 
 }
