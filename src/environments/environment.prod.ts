c8226d5e76ede2b38ca17b177ea77025e784be5a
fabian <fabr2008@hotmail.com>
2018-06-12 13:45:40 -0500

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {apiKey: "AIzaSyCASg-i2Jj_3vtwmZ5pCDFb8X-z3Me6sB0",
+    authDomain: "loginapp-a6234.firebaseapp.com",
+    databaseURL: "https://loginapp-a6234.firebaseio.com",
+    projectId: "loginapp-a6234",
+    storageBucket: "",
+    messagingSenderId: "505994004607"}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {apiKey: "AIzaSyCASg-i2Jj_3vtwmZ5pCDFb8X-z3Me6sB0",
+    authDomain: "loginapp-a6234.firebaseapp.com",
+    databaseURL: "https://loginapp-a6234.firebaseio.com",
+    projectId: "loginapp-a6234",
+    storageBucket: "",
+    messagingSenderId: "505994004607"}
 };
 
 /*
